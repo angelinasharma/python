2ae0068678f4ae08808e810785913c153c84752e
Angelina <beanwannasleep@gmail.com>
2024-03-15 10:04:37 +0530

    Trying to see if this works

---
+++ functions.py
@@ -0,0 +1,18 @@
+def first_function():
+    print("Hello there!")
+
+first_function()
+
+
+def second_function(fname):
+    print(fname + ".")
+
+second_function("name_1")
+second_function("name_2")
+second_function("name_3")
+
+#add a comment
+
+#add another comment
+
+#one more 
